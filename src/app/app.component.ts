47ed3350773e22307382c9a7efd4281fc0e5795e
jordan-reichelt <jjreichelt05@gmail.com>
2021-06-08 09:58:55 +1000

    updated styling to match tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes-is-awesome';
+  title = 'Tour of Heroes';
 }
